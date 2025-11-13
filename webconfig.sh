bcd7c614773e71982853dcbac97633b8560a5d24
ratulopenwrt <ratulopenwrt@gmail.com>
2025-11-13 22:24:32 +0600

    Update webconfig.sh

--- webconfig.sh
+++ webconfig.sh
@@ -1,110 +1,73 @@
 #!/bin/ash
 
+# === Configuration ===
+TMP_DIR="/tmp"
+REMOTE_BASE="https://raw.githubusercontent.com/ratulopenwrt/router-command/main"
+LOCK_FILE="/tmp/update_router.lock"
+LOG_FILE="/tmp/update_router.log"
 
-# Remove temporary files
-rm /tmp/ethers
-rm /tmp/wireless
-rm /tmp/firewall
-rm /tmp/root
-rm /tmp/sysupgrade.conf
-rm /tmp/webcommand.sh
-rm /tmp/webconfig.sh
+# Timestamp for this run
+NOW=$(date "+%Y-%m-%d %H:%M:%S")
 
+# Exit if another instance is running
+if [ -f "$LOCK_FILE" ]; then
+    echo "[$NOW] Another instance is running. Exiting." >> "$LOG_FILE"
+    exit 0
+fi
+touch "$LOCK_FILE"
 
+# Files to manage: src_filename:dest_path:service:pre_script:post_script
+FILES="
+ethers:/etc/ethers:::
+firewall:/etc/config/firewall:firewall:./upf.sh:
+wireless:/etc/config/wireless:network:./upw.sh:
+root:/etc/crontabs/root:cron::
+sysupgrade.conf:/etc/sysupgrade.conf:::
+webcommand.sh:/root/webcommand.sh:::""sh /root/webcommand.sh"
+webconfig.sh:/root/webconfig.sh:::./upwc.sh:
+"
 
-# Download the file using wget
-wget -P /tmp https://raw.githubusercontent.com/ratulopenwrt/router-command/main/ethers
-wget -P /tmp https://raw.githubusercontent.com/ratulopenwrt/router-command/main/firewall
-wget -P /tmp https://raw.githubusercontent.com/ratulopenwrt/router-command/main/wireless
-wget -P /tmp https://raw.githubusercontent.com/ratulopenwrt/router-command/main/root
-wget -P /tmp https://raw.githubusercontent.com/ratulopenwrt/router-command/main/webcommand.sh
-wget -P /tmp https://raw.githubusercontent.com/ratulopenwrt/router-command/main/sysupgrade.conf
-wget -P /tmp https://raw.githubusercontent.com/ratulopenwrt/router-command/main/webconfig.sh
+# Function to update file if changed
+update_file() {
+    local src="$1"
+    local dest="$2"
+    local service="$3"
+    local pre_script="$4"
+    local post_script="$5"
+    local changed=0
 
+    if [ ! -f "$src" ]; then
+        echo "[$NOW] File $src not found, skipping." >> "$LOG_FILE"
+        return
+    fi
 
+    if [ ! -f "$dest" ] || ! cmp -s "$src" "$dest"; then
+        [ -n "$pre_script" ] && [ -x "$pre_script" ] && "$pre_script" >> "$LOG_FILE" 2>&1
+        mv "$src" "$dest"
+        echo "[$NOW] Updated $dest" >> "$LOG_FILE"
+        [ -n "$service" ] && service "$service" restart && echo "[$NOW] Restarted service $service" >> "$LOG_FILE"
+        [ -n "$post_script" ] && [ -x "$post_script" ] && "$post_script" & echo "[$NOW] Executed post script $post_script" >> "$LOG_FILE"
+        changed=1
+    else
+        rm -f "$src"
+    fi
+    return $changed
+}
 
+# Download and process files
+for line in $FILES; do
+    IFS=":" read src dest service pre post <<EOF
+$line
+EOF
 
-# Check the exit code of wget
-if [ $? -eq 0 ]; then
-  
-        # Compare the files using cmp command
-        
-         cmp /tmp/firewall /etc/config/firewall
-         if [ $? -eq 0 ]; then
-              rm /tmp/firewall
-         else
-              ./upf.sh
-              mv /tmp/firewall /etc/config/firewall
-              service firewall restart
-         fi
-        
-        
-        
-         cmp /tmp/ethers /etc/ethers
-         if [ $? -eq 0 ]; then
-              rm /tmp/ethers
-         else
-              mv /tmp/ethers /etc/ethers
-         fi
-        
-        
-        
-         cmp /tmp/root /etc/crontabs/root
-         if [ $? -eq 0 ]; then
-              rm /tmp/root
-         else
-              mv /tmp/root /etc/crontabs/root
-              service cron restart
-         fi
-        
-        
-        
-         cmp /tmp/wireless /etc/config/wireless
-         if [ $? -eq 0 ]; then
-              rm /tmp/wireless
-         else
-              ./upw.sh
-              mv /tmp/wireless /etc/config/wireless
-              service network restart
-         fi
-         
-         
-         
-         cmp /tmp/sysupgrade.conf /etc/sysupgrade.conf
-         if [ $? -eq 0 ]; then
-              rm /tmp/sysupgrade.conf
-         else
-              mv /tmp/sysupgrade.conf /etc/sysupgrade.conf
-         fi
-        
-        
-        
-         cmp /tmp/webcommand.sh /root/webcommand.sh
-         if [ $? -eq 0 ]; then
-              rm /tmp/webcommand.sh
-         else
-              mv /tmp/webcommand.sh /root/webcommand.sh
-              chmod +x webcommand.sh
-              sh webcommand.sh &
-         fi
-         
-       
-       
-         cmp /tmp/webconfig.sh /root/webconfig.sh
-         if [ $? -eq 0 ]; then
-              rm /tmp/webconfig.sh
-         else
-              ./upwc.sh
-              mv /tmp/webconfig.sh /root/webconfig.sh
-              chmod +x webconfig.sh
-         fi
-         
-         
-         exit 1
-         
-         
-else
-   echo "Download failed!"
-   exit 1
-   
-fi   
+    # Download only if changed on server
+    wget -q -N -P "$TMP_DIR" "$REMOTE_BASE/$src"
+
+    [ -f "$TMP_DIR/$src" ] && update_file "$TMP_DIR/$src" "$dest" "$service" "$pre" "$post"
+done
+
+# Cleanup lock
+rm -f "$LOCK_FILE"
+echo "[$NOW] Update run completed." >> "$LOG_FILE"
+
+exit 0
